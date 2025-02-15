a3a4e6530e1b9612badda5455f1232cb5e9f649a
raj-Anand06 <btech10572.22@bitmesra.ac.in>
2025-02-15 21:34:45 +0530

    Update referralApi.js

--- src/api/referralApi.js
+++ src/api/referralApi.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: ''https://accredian-backend-task-4gq4.onrender.com'',  // Backend URL
+  baseURL: 'https://accredian-backend-task-4gq4.onrender.com',  // Backend URL
 });
 
 export const submitReferral = async (data) => {
   try {
-    const response = await api.post('/referrals', data);
+    const response = await api.post('/api/referrals', data);
     return response.data;
   } catch (error) {
     console.error('Error submitting referral:', error);
